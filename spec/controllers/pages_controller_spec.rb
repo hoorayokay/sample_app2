a93559d12d68c3a74373786b409ea9af616f059b
Rob <rob@trinorth.co.uk>
2012-11-29 12:21:18 +0000

    Guard added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
-
+  
 describe PagesController do
+
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      visit '/pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Home")
     end
   end
 
@@ -14,12 +23,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      visit '/pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      visit '/pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title} | About")
     end
   end
 end
